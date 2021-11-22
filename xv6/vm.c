fc177c6d73a70fde4dab4d8083fe6ca96498ab2f
Kevin20201 <kevin.yu.tu@gmail.com>
2021-11-21 20:01:07 -0800

    Lab3 Part 1

--- xv6/vm.c
+++ xv6/vm.c
@@ -319,10 +319,31 @@ copyuvm(pde_t *pgdir, uint sz)
   pte_t *pte;
   uint pa, i, flags;
   char *mem;
+  struct proc *curproc = myproc();
 
   if((d = setupkvm()) == 0)
     return 0;
   for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
+    }
+  }
+
+  uint stack_begin = KERNBASE - 1;
+  stack_begin = PGROUNDDOWN(stack_begin);
+
+  // Lab 3 Changes
+  for(i = stack_begin; i > stack_begin - (curproc->stack_pages) * PGSIZE; i -= PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
     if(!(*pte & PTE_P))
